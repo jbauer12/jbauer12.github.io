fac5e4a38711b56fa94f972088f474ac0aaa8204
jbauer12 <jonasbauerwi@gmail.com>
2023-10-21 19:20:27 +0200

    .

--- js/scripts.js
+++ js/scripts.js
@@ -218,18 +218,18 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbxd_gA7kQeMd7nmUv8eIL9EqlwOmNXf8iW4wpA2rUFPSXwgWaNavuV1fegptuFmdRQ/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
                         $('#alert-wrapper').html(alert_markup('danger', data.message));
-                    } else {
+                                            } else {
                         $('#alert-wrapper').html('');
                         $('#rsvp-modal').modal('show');
-                    }
+                                            }
                 })
                 .fail(function (data) {
-                    console.log(data);
+                                        console.log(data);
                     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
                 });
         }
